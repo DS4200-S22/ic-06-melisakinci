544e601ac368380f37fa289e1fef9635a89e9807
Melis Akinci <melis.akinci8@gmail.com>
2022-02-10 13:23:10 -0500

    ran out of time

--- main.js
+++ main.js
@@ -10,5 +10,9 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let randNum = Math.random();
+    console.log("random number produced"); 
+
+    let buttonDiv = docuemnt.getElementById("button-div");
+    buttonDiv.innerHTML = randNum;
 }
